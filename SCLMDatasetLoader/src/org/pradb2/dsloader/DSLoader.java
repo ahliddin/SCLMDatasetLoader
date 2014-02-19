942912c88ae28aa16825cc9602cbf4a779c2c121
unknown <ibrah01@IBRAH01W7.ca.com>
2014-02-19 16:42:50 +0100

    Added FTP library. Now it's able to connect to z/OS. Issue: FTP library needs to be adapted to z/OS, otherwise it's not able to download datasets from z/OS to MS Windows.

--- SCLMDatasetLoader/src/org/pradb2/dsloader/DSLoader.java
+++ SCLMDatasetLoader/src/org/pradb2/dsloader/DSLoader.java
@@ -1,5 +1,8 @@
 package org.pradb2.dsloader;
 
+import it.sauronsoftware.ftp4j.FTPException;
+
+import java.io.File;
 import java.util.logging.Logger;
 
 import org.eclipse.swt.widgets.DirectoryDialog;
@@ -14,6 +17,7 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.pradb2.exceptions.DSLoaderException;
+import org.eclipse.swt.widgets.Composite;
 
 public class DSLoader {
 	private static Logger LOGGER = Logger.getLogger("Global Logger"); 
@@ -21,14 +25,20 @@ public class DSLoader {
 	Display display;
 	Shell shell;
 	private Text txtSclmLevel;
-	private Text txtMember;
 	private Text txtType;
+	private Text txtMember;
 	private Text txtSaveTo;
 	private Text txtProgressLog;
 	private Button btnBrowse;
 	private Label lblType;
 	private Label lblMember;
 	private Button btnPull;
+	private Text txtUsername;
+	private Label lblUsername;
+	private Label lblPassword;
+	private Text txtPassword;
+	private Composite composite;
+	final private String SERVER = "usilca31.ca.com";
 	
 	
 	public DSLoader() {
@@ -51,29 +61,139 @@ public class DSLoader {
 	
 	private void setUpShell() {
 		shell.setText("SCLM Dataset Loader");
-		shell.setSize(571 , 369);
-		GridLayout globalShellLayout = new GridLayout(4, false);
+		shell.setSize(628 , 446);
+		GridLayout globalShellLayout = new GridLayout(5, false);
 		shell.setLayout(globalShellLayout);
 	}
 	
-	private void setUpLabel (Label label, GridData gdLayout,  String labelText) {
+	private void setUpShellComponents() {
+		composite = new Composite(shell, SWT.NONE);
+		GridData gd_composite = new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1);
+		gd_composite.widthHint = 300;
+		composite.setLayoutData(gd_composite);
+		composite.setLayout(new GridLayout(3, false));
 		
-		label.setLayoutData(gdLayout);
-		label.setText(labelText);
+		Label lblSclmLevel = new Label(composite, SWT.NONE);
+		lblSclmLevel.setAlignment(SWT.RIGHT);
+		lblSclmLevel.setText("SCLM Level: ");
+		
+		txtSclmLevel = new Text(composite, SWT.BORDER);
+		txtSclmLevel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		new Label(composite, SWT.NONE);
+		
+		lblType = new Label(composite, SWT.NONE);
+		lblType.setAlignment(SWT.RIGHT);
+		lblType.setText("Type:");
+		
+		txtType = new Text(composite, SWT.BORDER);
+		GridData gd_txtType = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_txtType.widthHint = 50;
+		txtType.setLayoutData(gd_txtType);
+		new Label(composite, SWT.NONE);
+		
+		lblMember = new Label(composite, SWT.NONE);
+		lblMember.setAlignment(SWT.RIGHT);
+		lblMember.setText("Member:");
+		
+		txtMember = new Text(composite, SWT.BORDER);
+		GridData gd_txtMember = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_txtMember.widthHint = 60;
+		txtMember.setLayoutData(gd_txtMember);
+		new Label(composite, SWT.NONE);
+		
+		Label lblSaveTo = new Label(composite, SWT.NONE);
+		lblSaveTo.setAlignment(SWT.RIGHT);
+		lblSaveTo.setText("Save To:");
+		
+		txtSaveTo = new Text(composite, SWT.BORDER);
+		txtSaveTo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		btnBrowse = new Button(composite, SWT.NONE);
+		btnBrowse.setText("Browse");
+		
+		lblUsername = new Label(shell, SWT.NONE);
+		lblUsername.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, false, false, 1, 1));
+		lblUsername.setText("Username:");
+		
+		txtUsername = new Text(shell, SWT.BORDER);
+		GridData gd_txtUsername = new GridData(SWT.FILL, SWT.TOP, false, false, 1, 1);
+		gd_txtUsername.widthHint = 70;
+		txtUsername.setLayoutData(gd_txtUsername);
+		
+		lblPassword = new Label(shell,  SWT.NONE);
+		lblPassword.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, false, false, 1, 1));
+		lblPassword.setText("Password:");
+		
+		txtPassword = new Text(shell, SWT.PASSWORD | SWT.BORDER);
+		GridData gd_txtPassword = new GridData(SWT.FILL, SWT.TOP, false, false, 1, 1);
+		gd_txtPassword.widthHint = 70;
+		txtPassword.setLayoutData(gd_txtPassword);
+		
+		txtProgressLog = new Text(shell, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+		txtProgressLog.setEditable(false);
+		txtProgressLog.setEnabled(false);
+		txtProgressLog.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 4, 1));
+		
+		btnPull = new Button(shell, SWT.NONE);
+		GridData gd_btnPull = new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1);
+		gd_btnPull.widthHint = 70;
+		btnPull.setLayoutData(gd_btnPull);
+		btnPull.setText("Pull");
+	}
+
+	private void checkInputFields() throws DSLoaderException {
+		if (txtSclmLevel.getText() == "") { 
+			throw (new DSLoaderException("Empty SCLM level"));
+		}
+		
+		if (txtType.getText() == "") { 
+			throw (new DSLoaderException("Empty member"));
+		}
+
+		if (txtMember.getText() == "") { 
+			throw (new DSLoaderException("Empty type name"));
+		}
+		
+		File dir = new File(txtSaveTo.getText());
+		if (txtSaveTo.getText() == "" && !dir.isDirectory()) { 
+			throw (new DSLoaderException("Directory is not specified correctly\n"));
+		}
+
 	}
 	
-//	private void setUpText (Text text, GridData gdData) {
-//		
-//		text.setLayoutData(gdData);
-//	}
-//
-//	public void displayFiles(Text txtSaveTo, String[] files) {
-//		for (int i = 0; files != null && i < files.length; i++) {
-//			txtSaveTo.setText(files[i]);
-//			txtSaveTo.setEditable(true);
-//		}
-//	}
-	private void defBtnBrowse () {
+	private void pullData (DSLoaderFTPClient ftpClient) throws DSLoaderException  {
+		String fullFilePath = txtSaveTo.getText() + "\\" + txtMember.getText() + "." + txtType.getText(); 
+		File localFile = new File (fullFilePath);
+		
+		try {
+			ftpClient.changeDirectory(txtSclmLevel.getText());
+		}
+		catch (FTPException e) {
+			txtProgressLog.append("changeDirectory FTPexception: " + e.getMessage() + "\n");
+			return;
+		}
+		catch (Exception e) {
+			txtProgressLog.append("changeDirectory Exception: " + e.getMessage() + "\n");
+			return;
+		}
+		
+		try {
+			ftpClient.download(fullFilePath, localFile);
+		}
+		catch (FTPException e) {
+			txtProgressLog.append("Download FTPexception: " + e.getMessage() + "\n");
+			return;
+		}
+		catch (Exception e) {
+			txtProgressLog.append("Download Exception: " + e.getMessage() + "\n");
+			return;
+		}
+		
+		
+		txtProgressLog.append("Pulling some data \n");
+	}
+	
+	private void defineBtnBrowse () {
 		btnBrowse.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
 				DirectoryDialog dialog = new DirectoryDialog(shell, SWT.NULL);
@@ -84,141 +204,71 @@ public class DSLoader {
 				if (path != null) {
 					txtSaveTo.setText(path.toString());
 					LOGGER.info("Directory name: " + txtSaveTo.getText());
-//					File file = new File(path);
-//					System.out.println (file.getName());
-//					
-//					if (file.isFile())
-//						displayFiles(txtSaveTo, new String[] { file.toString() });
-//					else
-//						displayFiles(txtSaveTo, file.list());
 				}
 			}
 		});
 	}
-	private void checkInputFields() throws DSLoaderException {
-		if (txtSclmLevel.getText() == "") { 
-			throw (new DSLoaderException("Empty SCLM level"));
-		}
-		
-		if (txtMember.getText() == "") { 
-			throw (new DSLoaderException("Empty member"));
-		}
 
-		if (txtType.getText() == "") { 
-			throw (new DSLoaderException("Empty type name"));
-		}
-
-		if (txtSaveTo.getText() == "") { 
-			throw (new DSLoaderException("You have to define the directory"));
-		}
-
-	}
-	
-	private void defBtnPull () {
+	private void defineBtnPull ()  {
 		btnPull.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent event) {
 				try {
 					checkInputFields();
 				}
 				catch (DSLoaderException exception) {
+//					throw (new DSLoaderException(exception.getMessage() + "\n"));
 					txtProgressLog.setText(exception.getMessage() + "\n");
+					return;
 				}
+				
+				DSLoaderFTPClient ftpClient = new DSLoaderFTPClient();
+				
+				try {
+					ftpClient.connect(SERVER);
+					ftpClient.login(txtUsername.getText(), txtPassword.getText());
+				}
+				catch (FTPException e) {
+					txtProgressLog.append("FTP exception: " + e.getMessage() + "\n");
+					return;
+				}
+				catch (Exception e) {
+					txtProgressLog.append(e.getStackTrace().toString() + "\n");
+					return;
+				}
+				
+				//No exception was thrown while connecting
+				txtProgressLog.append("Connection successfull..\n");
+				
+				try {
+					pullData (ftpClient);
+				}
+				catch (DSLoaderException e) {
+					txtProgressLog.append(e.getMessage());
+					return;
+				}
+				finally {
+					if (ftpClient.isConnected()){
+						try {
+							ftpClient.disconnect(true);
+						}
+						catch (Exception e) {
+							txtProgressLog.append("Exception while disconnecting: " + e.getMessage() + "\n");
+						}
+					}
+				}
+				
 			}
 			
 		});
 	}
 	
-	
-	private void init () {
+	public void init () {
 		
 		setUpShell();
+		setUpShellComponents();
+		defineBtnBrowse ();
+		defineBtnPull();
 		
-		Label labelSclmLevel = new Label(shell, SWT.NONE);
-		labelSclmLevel.setAlignment(SWT.RIGHT);
-		GridData gd_labelSclmLevel = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_labelSclmLevel.widthHint = 90;
-		setUpLabel (labelSclmLevel,
-					gd_labelSclmLevel, 
-					"SCLM Level:");
-		
-		txtSclmLevel = new Text(shell, SWT.BORDER);
-		GridData gdText = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gdText.widthHint = 150;
-		gdText.minimumWidth = 50;
-		txtSclmLevel.setLayoutData(gdText);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		
-		lblType = new Label(shell, SWT.NONE);
-		lblType.setAlignment(SWT.RIGHT);
-		GridData gd_lblType = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_lblType.widthHint = 90;
-		lblType.setLayoutData(gd_lblType);
-		lblType.setText("Type:");
-		
-		txtMember = new Text(shell, SWT.BORDER);
-		GridData gd_text = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
-		gd_text.widthHint = 150;
-		txtMember.setLayoutData(gd_text);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		
-		lblMember = new Label(shell, SWT.NONE);
-		lblMember.setAlignment(SWT.RIGHT);
-		GridData gd_lblMember = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_lblMember.widthHint = 90;
-		lblMember.setLayoutData(gd_lblMember);
-		lblMember.setText("Member:");
-		
-		txtType = new Text(shell, SWT.BORDER);
-		GridData gd_text_1 = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
-		gd_text_1.widthHint = 150;
-		txtType.setLayoutData(gd_text_1);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		
-		Label labelSaveTo = new Label(shell, SWT.NONE);
-		labelSaveTo.setAlignment(SWT.RIGHT);
-		GridData gd_labelSaveTo = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_labelSaveTo.widthHint = 90;
-		labelSaveTo.setLayoutData(gd_labelSaveTo);
-		labelSaveTo.setText("Save To:");
-		setUpLabel (labelSaveTo,
-					new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1),
-					"Save to:");
-		
-		txtSaveTo = new Text(shell, SWT.BORDER);
-		GridData gd_txtSaveTo = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
-		gd_txtSaveTo.widthHint = 200;
-		txtSaveTo.setLayoutData(gd_txtSaveTo);
-		
-		btnBrowse = new Button(shell, SWT.NONE);
-		GridData gd_btnBrowse = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_btnBrowse.widthHint = 90;
-		btnBrowse.setLayoutData(gd_btnBrowse);
-		btnBrowse.setText("Browse");
-		
-		btnPull = new Button(shell, SWT.NONE);
-		GridData gd_btnPull = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_btnPull.widthHint = 89;
-		btnPull.setLayoutData(gd_btnPull);
-		btnPull.setText("Pull");
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		
-		txtProgressLog = new Text(shell, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
-		txtProgressLog.setEditable(false);
-		txtProgressLog.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 3, 1));
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		
-		defBtnBrowse ();
-		defBtnPull();
 	}
 
 	public static void main (String[] args) {
---
+++ SCLMDatasetLoader/src/org/pradb2/dsloader/DSLoaderFTPClient.java
@@ -0,0 +1,7 @@
+package org.pradb2.dsloader;
+
+import it.sauronsoftware.ftp4j.FTPClient;
+
+public class DSLoaderFTPClient extends FTPClient {
+
+}
